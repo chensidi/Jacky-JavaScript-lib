29c1306ec53cd1b17006f1dc971cb742b08a7f78
Jacky Cheung <2541012495@qq.com>
2023-01-20 18:07:57 +0800

    feat: 新增实现几种number相关的方法

--- __test__/num.test.ts
+++ __test__/num.test.ts
@@ -2,7 +2,7 @@
   测试number实现方法
 */
 
-const { oNumIsFinite, oNumIsInteger } = require('../dist/index')
+const { oNumIsFinite, oNumIsInteger, oNumEqual, oNumIsNaN, oNumToString } = require('../dist/index')
 
 describe('测试isFinite方法', () => {
   test('常规测试', () => {
@@ -33,43 +33,100 @@ describe('测试isFinite方法', () => {
 
 describe('测试number的isInteger方法', () => {
   test('常规测试', () => {
+    expect(oNumIsInteger(0)).toBe(true)
+  })
+  test('常规测试2', () => {
+    expect(oNumIsInteger(-100000)).toBe(true)
+  })
+  test('常规测试3', () => {
+    expect(oNumIsInteger(0.1)).toBe(false)
+  })
+  test('常规测试4', () => {
+    expect(oNumIsInteger(Math.PI)).toBe(false)
+  })
+  test('常规测试5', () => {
+    expect(oNumIsInteger(Infinity)).toBe(false)
+  })
+  test('常规测试6', () => {
+    expect(oNumIsInteger(-Infinity)).toBe(false)
+  })
+  test('常规测试7', () => {
+    expect(oNumIsInteger('10')).toBe(false)
+  })
+  test('常规测试8', () => {
+    expect(oNumIsInteger([1])).toBe(false)
+  })
+})
+
+describe('测试oNumEqual方法', () => {
+  test('常规测试', () => {
+    expect(oNumEqual(0.1 + 0.2, 0.3)).toBe(true)
+  })
+  test('常规测试2', () => {
+    expect(oNumEqual(0.1 + 0.2, 0.31)).toBe(false)
+  })
+  test('常规测试3', () => {
+    expect(oNumEqual(true, 1)).toBe(true)
+  })
+})
+
+describe('测试Number的isNaN方法', () => {
+  test('常规测试', () => {
     expect(
-      oNumIsInteger(0)
+      oNumIsNaN(NaN)
     ).toBe(true)
   })
   test('常规测试2', () => {
     expect(
-      oNumIsInteger(-100000)
-    ).toBe(true)
+      oNumIsNaN('1')
+    ).toBe(false)
   })
   test('常规测试3', () => {
     expect(
-      oNumIsInteger(0.1)
+      oNumIsNaN('100F')
     ).toBe(false)
+  })
+  test('常规测试3', () => {
+    expect(
+      oNumIsNaN(0 / 0)
+    ).toBe(true)
+  })
+})
+
+describe('测试Number的toString方法', () => {
+  test('常规测试', () => {
+    expect(
+      oNumToString(10, 2)
+    ).toBe('1010')
+  })
+  test('常规测试2', () => {
+    expect(
+      oNumToString(10, 16)
+    ).toBe('a')
+  })
+  test('常规测试3', () => {
+    expect(
+      oNumToString(147, 11)
+    ).toBe('124')
   })
   test('常规测试4', () => {
     expect(
-      oNumIsInteger(Math.PI)
-    ).toBe(false)
+      oNumToString(147, 30)
+    ).toBe('4r')
   })
   test('常规测试5', () => {
     expect(
-      oNumIsInteger(Infinity)
-    ).toBe(false)
+      oNumToString(147)
+    ).toBe('147')
   })
-  test('常规测试6', () => {
+  test('测试负数底数', () => {
     expect(
-      oNumIsInteger(-Infinity)
-    ).toBe(false)
+      oNumToString(-147, 30)
+    ).toBe('-4r')
   })
-  test('常规测试7', () => {
+  test('测试负数底数2', () => {
     expect(
-      oNumIsInteger('10')
-    ).toBe(false)
-  })
-  test('常规测试8', () => {
-    expect(
-      oNumIsInteger([1])
-    ).toBe(false)
+      oNumToString(-0xff, 2)
+    ).toBe('-11111111')
   })
 })
--- src/number/number.ts
+++ src/number/number.ts
@@ -26,7 +26,62 @@ function oNumIsInteger(num: number): boolean {
   return true
 }
 
+/* 
+isNaN
+number上的isNaN与全局isNaN相比，更严格，只有才NaN这一种值的时候，才会返回true，其余情况一律返回false
+而全局的isNaN，会进行隐式转换为number类型，然后再判断是否为NaN
+*/
+function oNumIsNaN(num: number) {
+  if (typeof num !== 'number') return false
+  return isNaN(num)
+}
+
+/* 
+isEqual
+Number身上原本没有提供这个方法，这是用来比较两个数字是否相等
+比如，0.1 + 0.2 == 0.3000000.....3
+这是由于精度问题导致，所以使用===操作符并不能得到预期效果
+这个函数就是一个实现方法
+*/
+
+function oNumEqual(n1: number, n2: number) {
+  return Math.abs(n1 - n2) <= Number.EPSILON
+}
+
+/* 
+  toString
+  该方法是Number原型链上提供，将目标数字转换成十进制
+  然后再次转换为对应进制数
+  本实现方法暂时没有实现浮点数的进制转换，仅实现对整数的进制转换
+*/
+function oNumToString(num: number, bit = 10) {
+  num = Number(num)
+  bit = Number(bit)
+  const flag = num < 0
+  num = Math.abs(num)
+  if (bit < 2 || bit > 36) {
+    throw '进制应该在2到36之间'
+  }
+  if (bit === 10) return num.toString()
+  const arr: Array<string | number> = []
+  while (num > 0) {
+    let mod: number | string = num % bit
+    if (mod > 9) {
+      mod = String.fromCodePoint(mod + 87)
+    }
+    arr.unshift(mod)
+    num = Math.floor(num / bit)
+  }
+  if (flag) {
+    return `-${arr.join('')}`
+  }
+  return arr.join('')
+}
+
 export {
   oNumIsFinite,
   oNumIsInteger,
+  oNumIsNaN,
+  oNumEqual,
+  oNumToString,
 }
